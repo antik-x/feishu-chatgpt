5e3ca025c231b418a2aa70703c920efedd531fb1
antik <jianyong.me@gmail.com>
2023-06-06 10:48:38 +0800

    Update lark_client.go

--- code/initialization/lark_client.go
+++ code/initialization/lark_client.go
@@ -2,12 +2,13 @@ package initialization
 
 import (
 	lark "github.com/larksuite/oapi-sdk-go/v3"
+	larkcore "github.com/larksuite/oapi-sdk-go/v3/core"
 )
 
 var larkClient *lark.Client
 
 func LoadLarkClient(config Config) {
-	larkClient = lark.NewClient(config.FeishuAppId, config.FeishuAppSecret)
+	larkClient = lark.NewClient(config.FeishuAppId, config.FeishuAppSecret, lark.WithLogLevel(larkcore.LogLevelDebug))
 }
 
 func GetLarkClient() *lark.Client {
